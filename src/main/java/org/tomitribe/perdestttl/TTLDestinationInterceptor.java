f5bcbb13e319ef3d1297b4a8887fe9a8119f7e46
Jonathan Gallimore <jon@jrg.me.uk>
2023-03-24 16:56:05 +0000

    License headers

--- src/main/java/org/tomitribe/perdestttl/TTLDestinationInterceptor.java
+++ src/main/java/org/tomitribe/perdestttl/TTLDestinationInterceptor.java
@@ -23,13 +23,10 @@ import org.apache.activemq.broker.region.Destination;
 import org.apache.activemq.broker.region.DestinationFilter;
 import org.apache.activemq.broker.region.DestinationInterceptor;
 import org.apache.activemq.broker.region.policy.DeadLetterStrategy;
-import org.apache.activemq.broker.region.policy.PolicyEntry;
-import org.apache.activemq.broker.region.policy.PolicyMap;
 import org.apache.activemq.broker.util.TimeStampingBrokerPlugin;
 import org.apache.activemq.command.ActiveMQDestination;
 import org.apache.activemq.command.ActiveMQMessage;
 import org.apache.activemq.command.Message;
-import org.apache.activemq.filter.DestinationMapEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
